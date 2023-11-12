01d51cd69414fd2250ad3493d1afbd0f29237f03
Xatrekak <xatrekak@gmail.com>
2023-11-11 23:42:30 -0500

    added DO files

---
+++ description.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Description for FastAPI redoc/docs screen.
+"""
+desc = """
+Simple [FastAPI](fastapi.tiangolo.com/) Digital Ocean [App Platform](https://do.co/3gBHLOn) Deploy example.
+Don't forget to view the [ReDoc](https://ntbg.app/redoc)
+
+I'm thrilled to introduce ntbg.app, a new free service that provides random anime wallpapers from wallhaven.cc, tailored to your preference for use with nightTab. Whether you're after something Safe for Work and kid friendly, a Sketchy piece that's is a little more interesting, or you're in the mood for something risqué, ntbg.app has got you covered!
+"""
