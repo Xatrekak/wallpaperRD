6865f01384b66f0df3074ff43002f6da97016f15
Xatrekak <xatrekak@gmail.com>
2023-11-11 23:56:13 -0500

    fixed name error

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -8,6 +8,4 @@ Using their Flask template: https://github.com/digitalocean/sample-flask
 """
 
 bind = "0.0.0.0:8080"
-workers = 4
-# Using Uvicorn's Gunicorn worker class
-worker_class = "uvicorn.workers.UvicornWorker"
+workers = 2
